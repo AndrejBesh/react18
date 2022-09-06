da20e208958a5d6ddb9b761e20396eb2fe71bfd9
AndrejBesh <beslievski.a@gmail.com>
2022-09-06 19:34:22 +0200

    tryed

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,11 @@
-import { useState } from "react"
-
 export const Dropdown = ({ days, label, onDaysOffHandler, dayoff }) => {
-    const [selectValue, setSelectedValue] = useState()
-    const dataSender = (value) => {
-        setSelectedValue(value)
-        onDaysOffHandler(selectValue)
-    }
+
 
     return (
         <div>
             <label>{label}</label>
             <br></br>
-            <select onChange={e => dataSender(e.target.value)}>
+            <select onChange={e => onDaysOffHandler(e.target.value)}>
                 {days.map((day, i) => (
                     <option key={i} value={day}>
                         {day}
